3ec800d53cd5bb0656713079f28af51f4b623d83
Prashant <singhprashant0314@gmail.com>
2022-06-03 12:55:01 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/betaone/secActivity.kt
@@ -0,0 +1,11 @@
+package com.example.betaone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class secActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sec)
+    }
+}
